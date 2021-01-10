2b3d10036c1654dd3d2fd2ab01925243331055d8
katiegirlsarah <69067202+katiegirlsarah@users.noreply.github.com>
2021-01-10 19:48:57 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,30 +5,27 @@ var hurl = "";
 var vidplaying = false;
 var vid = undefined;
 
-function setup() {
-  createCanvas(1280, 720);
-  fetch(serv)
+// SETUP
+fetch(serv)
+.then(x => x.text())
+.then(y => src = y);
+
+// ==========================
+
+// MAIN LOOP - TODO - Make this not a loop, and use non async fetch call! (probably XHR)
+
+if(src!=""&&htvid==""){
+  fetch(src)
   .then(x => x.text())
-  .then(y => src = y);
+  .then(y => htvid = y);
 }
 
-function draw() {
-  background(220);
-  text(src,20,20);
-  if(src!=""&&htvid==""){
-    fetch(src)
-    .then(x => x.text())
-    .then(y => htvid = y);
-  }
-  if(htvid!=""&&!vidplaying){
-    try{hurl = htvid.split("<video id=\"thisPlayer")[1].split("src=\"")[1].split("\" type")[0]}
-    catch(e){}
-  }
-  text(hurl,20,40);
-  if(hurl!=""&&!vidplaying){
-    vidplaying=true;
-    vid = createVideo(hurl);
-    
-  }
-  // try{image(vid, 0, 0);}catch(e){}
+if(htvid!=""&&!vidplaying){
+  try{hurl = htvid.split("<video id=\"thisPlayer")[1].split("src=\"")[1].split("\" type")[0]}
+  catch(e){}
+}
+
+if(hurl!=""&&!vidplaying){
+  vidplaying=true;
+  vid = hurl;
 }
